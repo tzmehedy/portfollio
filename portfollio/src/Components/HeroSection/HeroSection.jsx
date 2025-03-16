29c8a1cdc8de1d206d826a18bb969b87931d46a0
unknown <mehedytouhidurzaman@gmail.com>
2025-03-17 01:26:34 +0600

    The hero section is completed

--- portfollio/src/Components/HeroSection/HeroSection.jsx
+++ portfollio/src/Components/HeroSection/HeroSection.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import NavBar from '../NavBar/NavBar';
+import heroImg1 from "../../assets/images/HeroImage1.png"
+import { FiDownload } from "react-icons/fi";
+import { FaTwitter, FaLinkedinIn, FaGithub } from "react-icons/fa";
+import { FaFacebookF } from "react-icons/fa6";
 
 const HeroSection = () => {
     return (
@@ -8,10 +12,85 @@ const HeroSection = () => {
           <NavBar></NavBar>
         </div>
 
-        <div className='min-h-screen'>
+        <div className=" container mx-auto p-6 flex justify-between items-center gap-x-36 relative">
+          <div className="md:w-1/2 space-y-5 ">
+            <h3 className="text-[#DDDDDD] text-4xl font-bold">
+              I&apos;m Md Touhidur Zaman,
+            </h3>
+            <h1 className="bg-gradient-to-r from-[#8A54F7] to-[#E1D3FD] bg-clip-text text-transparent text-7xl">
+              MERN Stack Web Developer.
+            </h1>
+            <p className="text-[#DDDDDD] text-2xl">
+              I break down complex user experinece problems to create integritiy
+              focussed solutions that connect billions of people
+            </p>
 
-            
+            <div className="flex space-x-5">
+              <div>
+                <a
+                  href="https://drive.google.com/file/d/1rHxLUXTKJ37GX95Sdm2lZCjtGMhcfQyb/view?usp=sharing"
+                  target="blank"
+                >
+                  <button className="flex gap-x-2 items-center text-[#8750F7] text-xl px-6 py-2 border-2 border-[#8750F7] rounded-3xl cursor-pointer hover:bg-[#8750F7] hover:text-[#DDDDDD]">
+                    Download CV <FiDownload className=""></FiDownload>
+                  </button>
+                </a>
+              </div>
+              <div className="flex items-center space-x-3 text-[#8750F7] text-xl">
+                <div className="p-2 border-2 border-[#8750F7] rounded-full cursor-pointer transition-all duration-200 ease-in-out hover:bg-[#8750F7] hover:text-[#DDDDDD]">
+                  <FaTwitter></FaTwitter>
+                </div>
+                <div className="p-2 border-2 border-[#8750F7] rounded-full cursor-pointer transition-all duration-200 ease-in-out hover:bg-[#8750F7] hover:text-[#DDDDDD]">
+                  <a href="https://www.facebook.com/tz.mehedy" target="blank">
+                    <FaFacebookF></FaFacebookF>
+                  </a>
+                </div>
+                <div className="p-2 border-2 border-[#8750F7] rounded-full cursor-pointer transition-all duration-200 ease-in-out hover:bg-[#8750F7] hover:text-[#DDDDDD]">
+                  <a
+                    href="https://www.linkedin.com/in/md-touhidur-zaman/"
+                    target="blank"
+                  >
+                    <FaLinkedinIn></FaLinkedinIn>
+                  </a>
+                </div>
+                <div className="p-2 border-2 border-[#8750F7] rounded-full cursor-pointer transition-all duration-200 ease-in-out hover:bg-[#8750F7] hover:text-[#DDDDDD]">
+                  <a href="https://github.com/tzmehedy" target="blank">
+                    <FaGithub></FaGithub>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="md:w-1/2 ">
+            <img
+              className="transition-all duration-700 ease-in-out hover:rotate-6 w-full h-[600px] border-4 border-[#8750F7] border-dashed p-16 rounded-xl shadow-2xl bg-[#0C1013]"
+              src={heroImg1}
+              alt=""
+            />
+          </div>
+        </div>
 
+        <div className="flex justify-center gap-20 py-10">
+          <div className="flex items-center text-[#DDDDDD] font-bold space-x-5">
+            <div>
+              <h1 className="text-9xl">02</h1>
+            </div>
+            <div>
+              <p className="text-2xl">
+                Years of <br /> Experience
+              </p>
+            </div>
+          </div>
+          <div className="flex items-center text-[#DDDDDD] font-bold space-x-5">
+            <div>
+              <h1 className="text-9xl">60+</h1>
+            </div>
+            <div>
+              <p className="text-2xl">
+                Projects <br /> Completed
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     );
--- portfollio/src/Components/NavBar/NavBar.jsx
+++ portfollio/src/Components/NavBar/NavBar.jsx
@@ -3,33 +3,69 @@ import logo from "../../../public/Logo.png"
 
 const NavBar = () => {
     return (
-      <div className="flex items-center justify-between p-3">
-        <div className="flex items-center space-x-2">
-          <img src={logo} className="w-28 h-28 " alt="" />
+      <div className="flex items-center justify-between ">
+        <div className="flex items-center">
+          <img src={logo} className="w-28 h-28  " alt="" />
           <p className="text-white text-xl">mehedytouhidurzaman@gmail.com</p>
         </div>
         <div>
           <ul className="flex space-x-3 text-white text-lg items-center">
             <li>
-              <a href="#services">Services</a>
+              <a
+                href="#services"
+                className="relative px-0 py-2  after:absolute
+              after:left-0 after:bottom-0 after:w-0 after:h-0.5 after:bg-[#6B3EC6] after:transition-x after:duration-300 hover:after:w-full"
+              >
+                Services
+              </a>
             </li>
             <li>
-              <a href="#works">Works</a>
+              <a
+                className="relative px-0 py-2  after:absolute
+              after:left-0 after:bottom-0 after:w-0 after:h-0.5 after:bg-[#6B3EC6] after:transition-x after:duration-300 hover:after:w-full"
+                href="#works"
+              >
+                Works
+              </a>
             </li>
             <li>
-              <a href="#resume">Resume</a>
+              <a
+                className="relative px-0 py-2  after:absolute
+              after:left-0 after:bottom-0 after:w-0 after:h-0.5 after:bg-[#6B3EC6] after:transition-x after:duration-300 hover:after:w-full"
+                href="#resume"
+              >
+                Resume
+              </a>
             </li>
             <li>
-              <a href="#skills">Skills</a>
+              <a
+                className="relative px-0 py-2  after:absolute
+              after:left-0 after:bottom-0 after:w-0 after:h-0.5 after:bg-[#6B3EC6] after:transition-x after:duration-300 hover:after:w-full"
+                href="#skills"
+              >
+                Skills
+              </a>
             </li>
             <li>
-              <a href="#testimonials">Testimonials</a>
+              <a
+                className="relative px-0 py-2  after:absolute
+              after:left-0 after:bottom-0 after:w-0 after:h-0.5 after:bg-[#6B3EC6] after:transition-x after:duration-300 hover:after:w-full"
+                href="#testimonials"
+              >
+                Testimonials
+              </a>
             </li>
             <li>
-              <a href="#contact">Contact</a>
+              <a
+                className="relative px-0 py-2  after:absolute
+              after:left-0 after:bottom-0 after:w-0 after:h-0.5 after:bg-[#6B3EC6] after:transition-x after:duration-300 hover:after:w-full"
+                href="#contact"
+              >
+                Contact
+              </a>
             </li>
             <li>
-              <button className="transition-all duration-700 ease-in-out bg-gradient-to-r from-[#7746DB]/80 to-[#341A65]/90 px-10 py-3 rounded-full cursor-pointer  hover:from-[#341A65] hover:to-[#7746DB]">
+              <button className="transition-all duration-700 bg-gradient-to-r from-[#7746DB]/80 to-[#341A65]/90 px-10 py-3 rounded-full cursor-pointer  hover:from-[#341A65] hover:to-[#7746DB] ml-7">
                 Hire Me
               </button>
             </li>
